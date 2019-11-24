56f8cd9e9cda42ff25efcff2189a8fd618929d66
Michael <ehrlinge@fim.uni-passau.de>
2019-11-24 23:39:50 +0100

    adding ieee906

---
+++ mosaik_pypower_ieee906/ieee906.py
@@ -0,0 +1,78 @@
+import csv
+import os
+import arrow
+
+
+from mosaik_pypower import resource_db as rdb
+import mosaik_pypower_ieee906.mosaik_csv_cyclic
+
+sim_config = {
+    'CSV': {
+        'python': 'mosaik_pypower_ieee906.mosaik_csv_cyclic:CSV',
+    },
+    'PyPower': {
+        'python': 'mosaik_pypower.mosaik:PyPower',
+    }
+}
+
+DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss'
+TIME_FORMAT = 'HH:mm:ss'
+START = '2014-01-01 00:00:00'
+
+this_dir, this_filename = os.path.split(__file__)
+DATAFOLDER = this_dir + '/data/'
+SHAPE_CONFIG = DATAFOLDER + 'Loads.csv'
+
+
+def get_load_busses():
+    with open(SHAPE_CONFIG, newline='') as csvfile:
+        config_reader = csv.reader(csvfile, delimiter=',')
+        busses = ['bus_' + row[2] for row in list(config_reader)[3:]]
+    return busses
+
+
+def connect_ieee906(world, start_time=START):
+    """
+    This method connects a mosaik-pypower instance with the IEEE 906 Test Feeder and respective loads as mosaik-csv
+    simulators to the provided world.
+
+    :param world: mosaik world to connect the pypower and csv simulator
+    :param start_time: starting time, default 00:00:00
+    :return: reference to the grid and houses entities, in order to e.g. connect further simulators or write results to
+     a database.
+    """
+    # add simulators to world config
+    world.sim_config.update(sim_config)
+
+    # format and fix time
+    start_date = arrow.get(START, DATE_FORMAT)
+    start_time = arrow.get(start_time, DATE_FORMAT)
+    start_date = start_date.replace(hour=start_time.hour, minute=start_time.minute, second=start_time.second)
+
+    # Start power simulator
+    pypower = world.start('PyPower', step_size=60)
+
+    # Feed in the 11 kV level to mosaik-pypower
+    rdb.base_mva[11] = 1
+
+    # Instantiate ieee 906 grid models
+    grid = pypower.Grid(gridfile=DATAFOLDER + 'ieee906.json').children
+    buses = filter(lambda e: e.type == 'PQBus', grid)
+    buses = {b.eid.split('-')[1]: b for b in buses}
+
+    # start and connect household loads from profiles
+    houses = []
+    with open(SHAPE_CONFIG, newline='') as csvfile:
+        config_reader = csv.reader(csvfile, delimiter=',')
+
+        i = 1
+        for row in list(config_reader)[3:]:
+            bus = 'bus_' + row[2]
+            shape_file = DATAFOLDER + 'shapes/Load_Profile_pq_' + str(i) + '.csv'
+
+            shapeSim = world.start('CSV', sim_start=start_date.format(), datafile=shape_file)
+            loadEntity = shapeSim.Load.create(1)[0]
+            world.connect(loadEntity, buses[bus], 'P', 'Q')
+            houses.append(loadEntity)
+            i = i + 1
+    return grid, houses
---
+++ mosaik_pypower_ieee906/mosaik_csv_cyclic.py
@@ -0,0 +1,154 @@
+import arrow
+
+import mosaik_api
+
+__version__ = '1.0.2'
+
+DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss'
+# cyclic data from csv files
+YEAR = 2014
+MONTH = 1
+DAY = 1
+
+
+class CSV(mosaik_api.Simulator):
+    def __init__(self):
+        super().__init__({'models': {}})
+        self.start_date = None
+        self.datafile = None
+        self.next_row = None
+        self.modelname = None
+        self.attrs = None
+        self.eids = []
+        self.cache = None
+
+        self.filename = None
+
+    def init(self, sid, sim_start, datafile):
+        self.start_date = arrow.get(sim_start)
+        # set date to date of the files
+        self.start_date.replace(year=YEAR, month=MONTH, day=DAY)
+        self.next_date = self.start_date
+
+        self.filename = datafile
+
+        self.attrs = self._init_csv_file()
+
+        self.meta['models'][self.modelname] = {
+            'public': True,
+            'params': [],
+            'attrs': self.attrs,
+        }
+
+        return self.meta
+
+    def create(self, num, model):
+        if model != self.modelname:
+            raise ValueError('Invalid model "%s" % model')
+
+        start_idx = len(self.eids)
+        entities = []
+        for i in range(num):
+            eid = '%s_%s' % (model, i + start_idx)
+            entities.append({
+                'eid': eid,
+                'type': model,
+                'rel': [],
+            })
+            self.eids.append(eid)
+        return entities
+
+    def step(self, time, inputs=None):
+        data = self.next_row
+        if data is None:
+            raise IndexError('End of CSV file reached.')
+
+        # Check date
+        date = data[0]
+        timeInput = time
+        hr = 0
+        min = 0
+        if time / 3600 >= 1:
+            hr = int(time / 3600)
+            time = time - (hr * 3600)
+
+        if time / 60 >= 1:
+            min = int(time / 60)
+            time = time - (min * 60)
+
+        sec = time
+        time = timeInput
+        expected_date = self.start_date.replace(hour=hr, minute=min, second=sec)
+        # set expected date always to start date
+        expected_date = expected_date.replace(year=YEAR, month=MONTH, day=DAY)
+        if date != expected_date:
+            raise IndexError('Wrong date "%s", expected "%s"' % (
+                date.format(DATE_FORMAT),
+                expected_date.format(DATE_FORMAT)))
+
+        # Put data into the cache for get_data() calls
+        self.cache = {}
+        for attr, val in zip(self.attrs, data[1:]):
+            self.cache[attr] = float(val)
+
+        self._read_next_row()
+        if self.next_row is not None:
+            return time + 60 #(self.next_row[0].timestamp - date.timestamp)
+        else:
+            return float('inf')
+
+    def get_data(self, outputs):
+        data = {}
+        for eid, attrs in outputs.items():
+            if eid not in self.eids:
+                raise ValueError('Unknown entity ID "%s"' % eid)
+
+            data[eid] = {}
+            for attr in attrs:
+                data[eid][attr] = self.cache[attr]
+
+        return data
+
+    def _init_csv_file(self):
+        self.datafile = open(self.filename)
+        self.modelname = next(self.datafile).strip()
+
+        # Get attribute names and strip optional comments
+        attrs = next(self.datafile).strip().split(',')[1:]
+        for i, attr in enumerate(attrs):
+            try:
+                # Try stripping comments
+                attr = attr[:attr.index('#')]
+            except ValueError:
+                pass
+            attrs[i] = attr.strip()
+        self.attrs = attrs
+
+        # Check start date
+        self._read_next_row()
+        if self.start_date < self.next_row[0]:
+            raise ValueError('Start date "%s" not in CSV file.' %
+                             self.start_date.format(DATE_FORMAT))
+        while self.start_date > self.next_row[0]:
+            self._read_next_row()
+            if self.next_row is None:
+                raise ValueError('Start date "%s" not in CSV file.' %
+                                 self.start_date.format(DATE_FORMAT))
+
+        return attrs
+
+    def _read_next_row(self):
+        try:
+            self.next_row = next(self.datafile).strip().split(',')
+            self.next_row[0] = arrow.get(self.next_row[0], DATE_FORMAT)
+        except StopIteration:
+            self.next_row = None
+
+        # end of file reached -> start from beginning with same time
+        if self.next_row is None:
+            self.datafile.close()
+            self._init_csv_file()
+
+
+def main():
+    return mosaik_api.start_simulation(CSV(), 'mosaik-csv simulator')
